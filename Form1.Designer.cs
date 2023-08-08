ded460e7bc117b458b40bd8f1f7469004b11db98
Saeed Lahmdi <saeed_99_per@outlook.com>
2023-08-08 22:35:07 +0300

    few changes in the designe

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -117,129 +117,129 @@
             // 
             // button1
             // 
-            this.button1.BackColor = System.Drawing.Color.Transparent;
+            this.button1.BackColor = System.Drawing.Color.Black;
             this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
             this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button1.Image = global::Tic_Tac_Toe_Game.Properties.Resources.QuestionMark;
-            this.button1.Location = new System.Drawing.Point(380, 70);
+            this.button1.Location = new System.Drawing.Point(378, 70);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(124, 117);
             this.button1.TabIndex = 0;
             this.button1.Tag = "?";
             this.button1.UseVisualStyleBackColor = false;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button1.Click += new System.EventHandler(this.button_Click);
             // 
             // button4
             // 
-            this.button4.BackColor = System.Drawing.Color.Transparent;
+            this.button4.BackColor = System.Drawing.Color.Black;
             this.button4.Cursor = System.Windows.Forms.Cursors.Hand;
             this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button4.Image = global::Tic_Tac_Toe_Game.Properties.Resources.QuestionMark;
-            this.button4.Location = new System.Drawing.Point(380, 217);
+            this.button4.Location = new System.Drawing.Point(378, 211);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(124, 117);
             this.button4.TabIndex = 3;
             this.button4.Tag = "?";
             this.button4.UseVisualStyleBackColor = false;
-            this.button4.Click += new System.EventHandler(this.button4_Click);
+            this.button4.Click += new System.EventHandler(this.button_Click);
             // 
             // button5
             // 
-            this.button5.BackColor = System.Drawing.Color.Transparent;
+            this.button5.BackColor = System.Drawing.Color.Black;
             this.button5.Cursor = System.Windows.Forms.Cursors.Hand;
             this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button5.Image = global::Tic_Tac_Toe_Game.Properties.Resources.QuestionMark;
-            this.button5.Location = new System.Drawing.Point(569, 217);
+            this.button5.Location = new System.Drawing.Point(567, 211);
             this.button5.Name = "button5";
             this.button5.Size = new System.Drawing.Size(124, 117);
             this.button5.TabIndex = 4;
             this.button5.Tag = "?";
             this.button5.UseVisualStyleBackColor = false;
-            this.button5.Click += new System.EventHandler(this.button5_Click);
+            this.button5.Click += new System.EventHandler(this.button_Click);
             // 
             // button2
             // 
-            this.button2.BackColor = System.Drawing.Color.Transparent;
+            this.button2.BackColor = System.Drawing.Color.Black;
             this.button2.Cursor = System.Windows.Forms.Cursors.Hand;
             this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button2.Image = global::Tic_Tac_Toe_Game.Properties.Resources.QuestionMark;
-            this.button2.Location = new System.Drawing.Point(569, 73);
+            this.button2.Location = new System.Drawing.Point(567, 73);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(124, 117);
             this.button2.TabIndex = 1;
             this.button2.Tag = "?";
             this.button2.UseVisualStyleBackColor = false;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.button2.Click += new System.EventHandler(this.button_Click);
             // 
             // button3
             // 
-            this.button3.BackColor = System.Drawing.Color.Transparent;
+            this.button3.BackColor = System.Drawing.Color.Black;
             this.button3.Cursor = System.Windows.Forms.Cursors.Hand;
             this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button3.Image = global::Tic_Tac_Toe_Game.Properties.Resources.QuestionMark;
-            this.button3.Location = new System.Drawing.Point(762, 73);
+            this.button3.Location = new System.Drawing.Point(760, 73);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(124, 117);
             this.button3.TabIndex = 2;
             this.button3.Tag = "?";
             this.button3.UseVisualStyleBackColor = false;
-            this.button3.Click += new System.EventHandler(this.button3_Click);
+            this.button3.Click += new System.EventHandler(this.button_Click);
             // 
             // button6
             // 
-            this.button6.BackColor = System.Drawing.Color.Transparent;
+            this.button6.BackColor = System.Drawing.Color.Black;
             this.button6.Cursor = System.Windows.Forms.Cursors.Hand;
             this.button6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button6.Image = global::Tic_Tac_Toe_Game.Properties.Resources.QuestionMark;
-            this.button6.Location = new System.Drawing.Point(762, 217);
+            this.button6.Location = new System.Drawing.Point(760, 211);
             this.button6.Name = "button6";
             this.button6.Size = new System.Drawing.Size(124, 117);
             this.button6.TabIndex = 5;
             this.button6.Tag = "?";
             this.button6.UseVisualStyleBackColor = false;
-            this.button6.Click += new System.EventHandler(this.button6_Click);
+            this.button6.Click += new System.EventHandler(this.button_Click);
             // 
             // button7
             // 
-            this.button7.BackColor = System.Drawing.Color.Transparent;
+            this.button7.BackColor = System.Drawing.Color.Black;
             this.button7.Cursor = System.Windows.Forms.Cursors.Hand;
             this.button7.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button7.Image = global::Tic_Tac_Toe_Game.Properties.Resources.QuestionMark;
-            this.button7.Location = new System.Drawing.Point(380, 357);
+            this.button7.Location = new System.Drawing.Point(378, 355);
             this.button7.Name = "button7";
             this.button7.Size = new System.Drawing.Size(124, 117);
             this.button7.TabIndex = 6;
             this.button7.Tag = "?";
             this.button7.UseVisualStyleBackColor = false;
-            this.button7.Click += new System.EventHandler(this.button7_Click);
+            this.button7.Click += new System.EventHandler(this.button_Click);
             // 
             // button8
             // 
-            this.button8.BackColor = System.Drawing.Color.Transparent;
+            this.button8.BackColor = System.Drawing.Color.Black;
             this.button8.Cursor = System.Windows.Forms.Cursors.Hand;
             this.button8.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button8.Image = global::Tic_Tac_Toe_Game.Properties.Resources.QuestionMark;
-            this.button8.Location = new System.Drawing.Point(569, 357);
+            this.button8.Location = new System.Drawing.Point(567, 355);
             this.button8.Name = "button8";
             this.button8.Size = new System.Drawing.Size(124, 117);
             this.button8.TabIndex = 7;
             this.button8.Tag = "?";
             this.button8.UseVisualStyleBackColor = false;
-            this.button8.Click += new System.EventHandler(this.button8_Click);
+            this.button8.Click += new System.EventHandler(this.button_Click);
             // 
             // button9
             // 
-            this.button9.BackColor = System.Drawing.Color.Transparent;
+            this.button9.BackColor = System.Drawing.Color.Black;
             this.button9.Cursor = System.Windows.Forms.Cursors.Hand;
             this.button9.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button9.Image = global::Tic_Tac_Toe_Game.Properties.Resources.QuestionMark;
-            this.button9.Location = new System.Drawing.Point(762, 357);
+            this.button9.Location = new System.Drawing.Point(760, 355);
             this.button9.Name = "button9";
             this.button9.Size = new System.Drawing.Size(124, 117);
             this.button9.TabIndex = 8;
             this.button9.Tag = "?";
             this.button9.UseVisualStyleBackColor = false;
-            this.button9.Click += new System.EventHandler(this.button9_Click);
+            this.button9.Click += new System.EventHandler(this.button_Click);
             // 
             // Form1
             // 
@@ -264,7 +264,6 @@
             this.Controls.Add(this.lbTitle);
             this.Name = "Form1";
             this.Text = "Form1";
-            this.Load += new System.EventHandler(this.Form1_Load);
             this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
             this.ResumeLayout(false);
             this.PerformLayout();
